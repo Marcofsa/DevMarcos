33cafae38935932129c21dea7c46ef72fd0948aa
Marcos Antonio Alves de Sá Filho <garenmanjador@hotmail.com>
2023-03-14 11:02:40 -0300

    New project + Uploading new archives.

---
+++ Alura projetos/Eventos-Delegates-Lambda/ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,65 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+        private readonly Agencia _agencia;
+
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+
+            _agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+            AtualizarCamposDeTexto();
+            AtualizarControles();
+        }
+
+        private void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtTelefone.Text = _agencia.Telefone;
+            txtEndereco.Text = _agencia.Endereco;
+            txtDescricao.Text = _agencia.Descricao;
+        }
+
+        private void AtualizarControles()
+        {
+
+           RoutedEventHandler dialogResultTrue = delegate (object o, RoutedEventArgs e)
+            {
+               DialogResult = true;
+            };
+
+            RoutedEventHandler dialogResultFalse = delegate (object o, RoutedEventArgs e)
+            {
+                DialogResult = false;
+            };
+
+            var okEventHandler = dialogResultTrue + Fechar;
+            var cancelarEventHandler = dialogResultFalse + Fechar;
+
+            btnOk.Click += okEventHandler;
+            btnCancelar.Click += cancelarEventHandler;
+        } 
+        private void Fechar(object sender, EventArgs e) =>
+            Close();
+    }
+}
---
+++ Alura projetos/Eventos-Delegates-Lambda/ByteBank.Agencias/ListBox.cs
@@ -0,0 +1,37 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ByteBank.Agencias
+{
+    public class ListBox : System.Windows.Controls.ListBox
+    {
+        private readonly MainWindow _janelaMae;
+
+        public ListBox()
+        {
+        }
+
+        public ListBox(MainWindow janelaMae)
+        {
+            _janelaMae = janelaMae ?? throw new ArgumentNullException(nameof(janelaMae));
+        }
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+
+            var agenciaSelecionada = (Agencia)SelectedItem;
+
+            _janelaMae.txtNumero.Text = agenciaSelecionada.Numero;
+            _janelaMae.txtNome.Text = agenciaSelecionada.Nome;
+            _janelaMae.txtTelefone.Text = agenciaSelecionada.Telefone;
+            _janelaMae.txtEndereco.Text = agenciaSelecionada.Endereco;
+            _janelaMae.txtDescricao.Text = agenciaSelecionada.Descricao;
+        }
+    }
+}
